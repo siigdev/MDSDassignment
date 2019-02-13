04539aa3af8a09568c8185b7a52471a29d9fc045
siigdev <sebbahlol@gmail.com>
2019-02-13 19:47:20 +0100

    Testing

--- InternalDSL/dsl.py
+++ InternalDSL/dsl.py
@@ -1,1 +1,13 @@
-#Initial commit
+from fysom import *
+ 
+fsm = Fysom({'initial': 'awake',
+             'final': 'red',
+             'events': [
+                 {'name': 'wakeup', 'src': 'sleeping', 'dst': 'awake'},
+                 {'name': 'sleep',  'src': 'awake',   'dst': 'sleeping'}]})
+ 
+print(fsm.current)   # awake
+fsm.sleep()
+print(fsm.current)   # sleeping
+fsm.wakeup()
+print(fsm.current)   # awake
