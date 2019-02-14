78aefd89d0ee0ca181c9d2b8b2fbc60fa7756bfb
siigdev <sebbahlol@gmail.com>
2019-02-14 22:57:07 +0100

    Fixed with states

--- InternalDSL/dsl.py
+++ InternalDSL/dsl.py
@@ -1,12 +1,17 @@
+
+
+
 state("IDLE").
-    transition("START_PRESSED").to("DOOR_OPEN")
+    transition("START_PRESSED").to("DOOR_OPEN").setState("floor", 0)
 state("DOOR_OPEN").
     transition("CLOSED").to("GOING_UP").whenStateEquals("UP_CLICK")
     transition("CLOSED").to("IDLE").whenStateEquals("NULL")
 state("GOING_UP").
-    transition("ARRIVED").to("STOPPED_AT_FLOOR").whenStateEquals(FloorNumber)
+    transition("ARRIVED").to("STOPPED_AT_FLOOR").whenStateEquals("floor", selected)
+                        else.changeState("floor", +1)
 state("GOING_DOWN").
-    transition("ARRIVED").to("STOPPED_AT_FLOOR").whenStateEquals(FloorNumber=0)
+    transition("ARRIVED").to("STOPPED_AT_FLOOR").whenStateEquals("floor", 0).
+                        else.changeState("floor", -1)
 state("STOPPED_AT_FLOOR")
     transition("OPEN").to("DOOR_OPEN")
     transition("GO_DOWN").to("GOING_DOWN")
