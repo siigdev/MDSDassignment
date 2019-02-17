65e76bff493bd8e5c5d4639bd88f1c50bff7ec0c
siigdev <sebbahlol@gmail.com>
2019-02-17 21:34:52 +0100

    Cleaner

--- Example.py
+++ Example.py
@@ -2,5 +2,8 @@ from executor.Executor import Executor
 from builder.Builder import *
 
 
-Executor.View(0, Triangle().setColor("green")
+Executor.View(0, 
+    Triangle().setColor("green"),
+    Triangle().setColor("red"),
+    Triangle().setColor("blue")
 )
--- executor/Executor.py
+++ executor/Executor.py
@@ -1,14 +1,11 @@
 import tkinter
 
-class Executor(object):
-    def View(self, x):
+class Executor():
+    def View(self, *args):
 
         master = tkinter.Tk()
 
         canvas = tkinter.Canvas(master,height=250, width=300)
-
-        for element in canvas.getelements:
-            print("test")
         canvas.pack()
         master.mainloop()
         
