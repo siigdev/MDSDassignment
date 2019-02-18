13cb384b3395eb6c0b7e3bfeb8f7a46c03040ae2
siigdev <sebbahlol@gmail.com>
2019-02-18 23:18:11 +0100

    Cleaning 😼

--- executor/Executor.py
+++ executor/Executor.py
@@ -8,12 +8,3 @@ class Executor:
     def __init__(self, machine):
         self.machine = machine
         self.currentState = State("START")
-
-    def test(self, trigger):
-        for state in self.machine.states:
-            if state.name == self.currentState.name:
-                for transition in state.transitions:
-                    if transition.trigger == trigger:
-                        print("WE ARE HERE")
-                print("WE ARE NOT HERE")
-        print("NOT AT ALL")
